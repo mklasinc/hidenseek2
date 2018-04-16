de1112994280dd763eeb7ce8079789cb2ad3209c
Seongun <mk4908@nyu.edu>
2018-04-16 09:33:53 -0800

    sound testing

--- hw_photon/Assets/Scripts/NetworkManager.cs
+++ hw_photon/Assets/Scripts/NetworkManager.cs
@@ -20,6 +20,8 @@ public class NetworkManager : MonoBehaviour {
     public GameObject seekerprefab;
     public GameObject hiderprefab;
 
+    private bool spawningHider = false;
+
     //Array to hold the spawn points in the scene and an array to see which spawn points have been taken
     public Transform[] spawnPoints;
 	public bool[] spawnPointTaken;
@@ -113,6 +115,7 @@ public class NetworkManager : MonoBehaviour {
         }else {
             GameObject.Instantiate(hiderprefab, spawnLocation, Quaternion.identity);
 			Debug.Log ("spawn a hider!");
+            spawningHider = true;
         };
 
         //count the players, if there's 2 players begin the game
@@ -152,11 +155,13 @@ public class NetworkManager : MonoBehaviour {
 		GameObject headset = GameObject.Find ("Camera (eye)");
 		GameObject photonCube = PhotonNetwork.Instantiate(headsetcubeprefab.name, headset.transform.position, Quaternion.identity, 0);
 		photonCube.transform.SetParent (headset.transform);
+        photonCube.GetComponent<AudioSource>().Play();
+        spawningHider = false;
 
-		//Find the controllers and instantiate capsules ON NETOWRK -- set controllers as the parents of the capsules
-       
+        //Find the controllers and instantiate capsules ON NETOWRK -- set controllers as the parents of the capsules
+
         //Find left controller
-		GameObject controllerLeft = GameObject.Find ("Controller (left)/Model");
+        GameObject controllerLeft = GameObject.Find ("Controller (left)/Model");
 		Debug.Log (controllerLeft);
 		GameObject capsuleHandLeft = PhotonNetwork.Instantiate(capsulehand.name, controllerLeft.transform.position, Quaternion.identity, 0);
 		capsuleHandLeft.transform.SetParent (controllerLeft.transform);
