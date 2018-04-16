fcf5042a7af954a5b1c520002628a950c3aeca58
shantanu <njw275@nyu.edu>
2018-04-16 19:28:09 +0400

    added sound script to hider

---
+++ hw_photon/Assets/HiderSoundManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiderSoundManager : MonoBehaviour {
+
+	AudioSource s;
+	// Use this for initialization
+	void Start () {
+		s = gameObject.GetComponent<AudioSource> ();
+		s.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
