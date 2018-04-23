da2b86606315cdd8ee3fc048df675400941d99fa
Alejandra Trejo <atr313@nyu.edu>
2018-04-23 16:19:36 +0400

    add sound seeker

---
+++ v_2/Assets/Scripts/Seeker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeker : Photon.MonoBehaviour {
+	PhotonView photonView;
+	public AudioSource audioobj;
+	public AudioClip steps;
+
+	// Use this for initialization
+	void Start () {
+		photonView = PhotonView.Get (this);
+		audioobj = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (photonView.isMine) {
+			if(Input.GetKeyDown(KeyCode.S))
+				makeSound ();
+		}
+	}
+
+	[PunRPC] void makeSound(){
+		audioobj.Play ();
+	}
+}
