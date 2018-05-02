425f5ba34a863bdf43a5d056f066d5267e2cb406
Alejandra Trejo <atr313@nyu.edu>
2018-05-02 06:49:26 -0800

    add trigger sound when nearby

---
+++ v_2/Assets/Scripts/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+	GameObject netman;
+
+	public string sfx_name;
+	void OnTriggerEnter (Collider col){
+		//call network manager and give position and type(what kind of sound)
+		//find network manager
+		Debug.Log ("A collision was detected");
+		if(col.gameObject.name == "hsCube(Clone)")
+		{
+			netman = GameObject.Find ("Manager");
+			netman.GetComponent<NetworkManager> ().PlaySFX (col.gameObject.transform.position, sfx_name);
+		}
+
+	}
+}
